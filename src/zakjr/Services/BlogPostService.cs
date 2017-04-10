e769f9eed4ff82b2d85fa400048a80b7d322561c
Zak <zak.reynolds@zakjr.com>
2017-04-09 22:31:38 -0500

    Moved mock to a new implementation of IBlogPostService. Made BlogPostService build from db.

--- src/zakjr/Services/BlogPostService.cs
+++ src/zakjr/Services/BlogPostService.cs
@@ -1,8 +1,10 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net.Http;
 using System.Threading.Tasks;
+using zakjr.Data;
 using zakjr.Models;
 using zakjr.ViewModels;
 
@@ -11,6 +13,13 @@ namespace zakjr.Services
     public class BlogPostService : IBlogPostService
     {
 
+        private ApplicationDbContext _context { get; }
+
+        public BlogPostService(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
         public BlogPostViewModel GetBlogPost(int id)
         {
             throw new NotImplementedException();
@@ -18,139 +27,15 @@ namespace zakjr.Services
 
         public async Task<BlogPostViewModel> GetBlogPostAsync(int id)
         {
-            // TODO: This gets a random collection of ipsum
-            HttpClient hc = new HttpClient();
-            var ipsResponse3 = await hc.GetAsync("https://baconipsum.com/api/?type=meat-and-filler&sentences=3&format=text");
-            var ipsResponse5 = await hc.GetAsync("https://baconipsum.com/api/?type=meat-and-filler&sentences=5&format=text");
-            var ipsResponse1 = await hc.GetAsync("https://baconipsum.com/api/?type=meat-and-filler&sentences=1&format=text");
-            string ipsString1 = await ipsResponse1.Content.ReadAsStringAsync();
-            string ipsString3 = await ipsResponse3.Content.ReadAsStringAsync();
-            string ipsString5 = await ipsResponse5.Content.ReadAsStringAsync();
-
-
             var result = new BlogPostViewModel();
-            result.ThePost = new BlogPost()
+            result.ThePost = await _context.BlogPosts.FirstOrDefaultAsync(bp => bp.ID == id);
+            result.ThePost.ContentList = await _context.ContentChunks.Where(cc => cc.BlogPostID == id).ToListAsync();
+            result.ThePost.ContentList.Sort((a, b) => a.Sequence.CompareTo(b.Sequence));
+
+            result.TheComments = new CommentSection()
             {
-                ID = -1,
-                FeaturedImage = "http://localhost:55671/images/cover1.jpg",
-                Title = "A Test of Homestead",
-                Subtitle = "This is a mocked blog post-- remind Zak to link this up to the db!",
-                PostDate = DateTime.Now.AddDays(-45),
-                UpdatedDate = DateTime.Now,
-                Category = new Category()
-                {
-                    ID = -1,
-                    Name = "Homestead",
-                    Tagline = "A mocked category"
-                },
-                CategoryID = -1,
-                ContentList = new List<ContentChunk>()
-                {
-                    new TextContentChunk()
-                    {
-                        Content = ipsString3
-                    },
-                    new ImageContentChunk()
-                    {
-                        ID = 1,
-                        ImageUrl = "http://zakjr.com/blog/wp-content/uploads/2016/03/betsy-768x432.png",
-                        CanExpand = true,
-                        ImageCaption = "Mocked image",
-                        LoadingGradient = "linear-gradient(135deg, #627d4d 0%,#1f3b08 100%)"
-                    },
-                    new TextContentChunk()
-                    {
-                        Content = ipsString5
-                    },
-                    new TextContentChunk()
-                    {
-                        Content = ipsString1
-                    },
-                    new ImageContentChunk()
-                    {
-                        ID = 2,
-                        ImageUrl = "http://zakjr.com/blog/wp-content/uploads/2016/02/Homestead-0001-768x402.png",
-                        CanExpand = true,
-                        ImageCaption = "Mocked image",
-                        LoadingGradient = "linear-gradient(135deg, #627d4d 0%,#1f3b08 100%)"
-                    },
-                    new TextContentChunk()
-                    {
-                        Content = ipsString3
-                    },
-                    new CodeContentChunk()
-                    {
-                        Content = "int LifeUniverseEverything(string question) { return 42; }",
-                        CodeLanguage = "cs"
-                    },
-                    new TextContentChunk()
-                    {
-                        Content = ipsString5
-                    },
-                    new ImageContentChunk()
-                    {
-                        ID = 3,
-                        ImageUrl = "http://zakjr.com/blog/wp-content/uploads/2015/11/homestead-first-dialogue-1024x576.png",
-                        CanExpand = true,
-                        ImageCaption = "Mocked image",
-                        LoadingGradient = "linear-gradient(135deg, #627d4d 0%,#1f3b08 100%)"
-                    },
-                    new TextContentChunk()
-                    {
-                        Content = ipsString3
-                    }
-                },
-                Comments = new List<Comment>()
-                {
-                    new Comment() {
-                        Author = "Mock",
-                        Content = ipsString1,
-                        PostDate = DateTime.Now,
-                        Comments = new List<Comment>()
-                        {
-                            new Comment() {
-                                Author = "Mock",
-                                Content = ipsString3,
-                                PostDate = DateTime.Now
-                            }
-                        }
-                    },
-                    new Comment() {
-                        Author = "Mock",
-                        Content = ipsString1,
-                        PostDate = DateTime.Now
-                    },
-                    new Comment() {
-                        Author = "Mock",
-                        Content = ipsString3,
-                        PostDate = DateTime.Now,
-                        Comments = new List<Comment>()
-                        {
-                            new Comment() {
-                                Author = "Mock",
-                                Content = ipsString1,
-                                PostDate = DateTime.Now,
-                                Comments = new List<Comment>()
-                                {
-                                    new Comment() {
-                                        Author = "Mock",
-                                        Content = ipsString3,
-                                        PostDate = DateTime.Now
-                                    },
-                                    new Comment() {
-                                        Author = "Mock",
-                                        Content = ipsString5,
-                                        PostDate = DateTime.Now
-                                    }
-                                }
-                            }
-                        }
-                    }
-                }
-            };
-            result.TheComments = new CommentSection() {
-                HeaderText = "Convey a thought",
-                CommentList = result.ThePost.Comments
+                HeaderText = "Comments",
+                CommentList = await _context.Comments.Where(c => c.BlogPostID == id).Include(c => c.Comments).ToListAsync()
             };
             result.TheCover = new BlogPostCover()
             {
@@ -158,28 +43,28 @@ namespace zakjr.Services
                 Title = result.ThePost.Title,
                 TagLine = result.ThePost.Subtitle
             };
+
+            // TODO: This more elegantly
+            Category postCategory = await _context.Categories.FirstOrDefaultAsync(cat => cat.ID == result.ThePost.CategoryID);
             result.TheNavigation = new List<NavItem>()
             {
                 new NavItem()
                 {
-                    Url = "#",
-                    DisplayText = "Mock"
+                    Url = "/blog",
+                    DisplayText = "Home"
                 },
                 new NavItem()
                 {
-                    Url = "#",
-                    DisplayText = "Mock Cat"
+                    Url = "/blog/categories/" + postCategory.ID,
+                    DisplayText = postCategory.Name
                 },
                 new NavItem()
                 {
-                    Url = "#",
-                    DisplayText = "Current Page"
+                    Url = "/blog/post/" + result.ThePost.ID,
+                    DisplayText = result.ThePost.Title
                 }
             };
-
-
-            await Task.Delay(200);
-
+            
             return result;
         }
 
